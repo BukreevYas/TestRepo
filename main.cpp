7e827c288d545fdf2bd1d6f2425d6e36d81bdb4f
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-10-25 20:01:35 +0300

    Added multiply overflow error detection

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 bool isPyth(unsigned a, unsigned b, unsigned c);
 
@@ -14,9 +15,17 @@ int main()
 
     while (std::cin >> a)
     {
-        count += isPyth(a, b, c) ? 1 : 0;
-        c = b;
-        b = a;
+        try
+        {
+            count += isPyth(a, b, c) ? 1 : 0;
+            c = b;
+            b = a;
+        }
+        catch(const std::overflow_error& e)
+        {
+            std::cerr << "One of the entered number is too big.\n";
+            return 2;
+        }
     }
 
     if (std::cin.eof())
@@ -33,6 +42,13 @@ int main()
 
 bool isPyth(unsigned a, unsigned b, unsigned c)
 {
+    unsigned u_max = std::numeric_limits<unsigned>::max();
+
+    if (a > u_max / a || b > u_max / b || c > u_max / c)
+    {
+        throw std::overflow_error("isPyth");
+    }
+
     bool p = a * a == b * b + c * c;
     p = p || (b * b == a * a + c * c);
     p = p || (c * c == a * a + b * b);
